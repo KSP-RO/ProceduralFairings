9b7ee275f95c901e12d7fcc575ca11ddc1b6aff2
Clayel <videogamer0907@gmail.com>
2026-07-29 13:40:11 -0400

    actually commit to the right branch

--- Source/ProceduralFairings/FairingBase.cs
+++ Source/ProceduralFairings/FairingBase.cs
@@ -573,21 +573,22 @@ namespace Keramzit
 
         public void UpdateFairingSideDragCubes()
         {
-            if (GetFairingSides(part).FirstOrDefault() is ProceduralFairingSide p)
+            if (GetFairingSides(part)?.FirstOrDefault() is ProceduralFairingSide p)
                 DragCubeTool.UpdateDragCubes(p.part, p.ShapeKey, updateSymCounterparts: true);
         }
-        
+
         public void UpdateOpen()
         {
             Vector3 offsetAmount = openFairing ? EditorOpenOffset : Vector3.zero;
             List<ProceduralFairingSide> sides = GetFairingSides(part);
-            foreach (var side in sides)
-            {
-                StartCoroutine(side.SetOffset(side.meshPos + offsetAmount, openSpeed));
-            }
 
-            if (sides.Count > 0)
+            if (sides?.Count > 0)
             {
+                foreach (var side in sides)
+                {
+                    StartCoroutine(side.SetOffset(side.meshPos + offsetAmount, openSpeed));
+                }
+
                 StartCoroutine(WaitAndFireVesselModified(openSpeed));
             }
         }
